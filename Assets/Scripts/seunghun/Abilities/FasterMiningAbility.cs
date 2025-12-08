e6a1484cca56b743a2189350c96b529af24caa30
seunghun <seung2456@naver.com>
2025-12-08 21:16:49 +0900

    Feat: 특수자원 채굴량은 능력 적용X

--- Assets/Scripts/seunghun/Abilities/FasterMiningAbility.cs
+++ Assets/Scripts/seunghun/Abilities/FasterMiningAbility.cs
@@ -15,10 +15,13 @@ public class FasterMiningAbility : CrimerAbility
 
     public override MiningTickResult ProcessMiningTick(MineableResource node)
     {
+        float amount = node.amountPerTick;
+        if (node.resourceType != ResourceType.Special)
+            amount *= 1.5f;
         return new MiningTickResult
         {
             Type = node.resourceType,                   // 타입은 'Special'로 고정
-            Amount = node.amountPerTick * 1.5f
+            Amount = amount
         };
     }
 }
